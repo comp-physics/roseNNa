d21a4347f1e4a010f74aaf91bc6fed5d86d994c8
Ajay Bati <ajaybati@gmail.com>
2022-03-04 12:57:30 -0500

    lstm_cell 1.0

--- linearV3.f90
+++ linearV3.f90
@@ -49,37 +49,7 @@ END PROGRAM linear
 
 !=======================================================================================
 
-FUNCTION linear_layer(inp, weights, bias) result(output)
-    IMPLICIT NONE
 
-    real, intent(in) :: weights(:,:)
-    real, intent(in) :: inp(:)
-    real, intent(in) :: bias(:)
-    real :: output(size(weights, dim=1))
-
-    output = matmul(weights,inp) + bias
-END FUNCTION linear_layer
-
-subroutine lstm_cell(input, hid1, cell1, Whh, Wih, Bih, Bhh, hiddenOut, cellOut)
-    implicit none
-    real, intent(in), DIMENSION(:) :: input !== (n,1)
-    real, intent(in), DIMENSION(:) :: hid1 !==(m,1)
-    real, intent(in), DIMENSION(:) :: cell1 !==(m,1)
-    real, intent(in), DIMENSION(:,:) :: Whh !==(4m,m)
-    real, intent(in), DIMENSION(:,:) :: Wih !==(4m,n)
-    real, intent(in), DIMENSION(:) :: Bhh !==(4m,1)
-    real, intent(in), DIMENSION(:) :: Bih !==(4m,1)
-    real, intent(out), DIMENSION(size(hid1)) :: hiddenOut
-    real, intent(out), DIMENSION(size(cell1)) :: cellOut
-
-    real, DIMENSION(size(Whh, dim=1)) :: gates_out
-    integer :: chunk
-
-    chunk = size(Whh, dim=1) / 4
-    gates_out = matmul(input, Wih) + Bih + matmul(hid1, Whh) + Bhh
-
-    gates_out(1:chunk) = 
-    gates_out(chunk:chunk*2)
 
 
 
